e5891926e292b76b18a2dbe90e1024cd281d1763
liyameng131452 <liyamengyw@163.com>
2018-04-08 11:41:45 +0800

    二级分类模块完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -22,7 +22,6 @@ $(function(){
             currentPage=page;
             render();
           }
-
         })
       }
     })
@@ -38,7 +37,7 @@ $(function(){
         pageSize:100,
       },
       success:function(info){
-        console.log(info);
+        // console.log(info);
         var htmlStr=template('dropdownMenu',info);
         $('.dropdown-menu').html(htmlStr);
       }
@@ -50,8 +49,72 @@ $(function(){
     var txt=$(this).text();
     $('#droptext').text(txt);
     $('[name="categoryId"]').val(id);
+    $('#form').data('bootstrapValidator').updateStatus('categoryId','VALID');
+    
   })
-
+  //获取图片上传的地址
+  $('#fileUpload').fileupload({
+    dataType:'json',
+    done:function(e,data){
+      var picAddr=data.result.picAddr;
+      // console.log(picAddr);
+      $('#imgBox img').attr('src',picAddr);
+      $('[name="brandLogo"]').val(picAddr);
+      $('#form').data('bootstrapValidator').updateStatus('brandLogo','VALID');
+    }
+  })
+  //进行表单验证
+  $('#form').bootstrapValidator({
+    excluded: [],
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    fields:{
+      categoryId:{
+        validators:{
+          notEmpty:{
+            message:'请输入一级分类'
+          }
+        }
+      },
+      brandName:{
+        validators:{
+          notEmpty:{
+            message:'请输入二级分类'
+          }
+        }
+      },
+      brandLogo:{
+        validators:{
+          notEmpty:{
+            message:'请上传图片'
+          }
+        }
+      }
+    }
+  })
+  //进行验证成功后的ajax请求
+  $('#form').on('success.form.bv',function(){
+    $.ajax({
+      url:'/category/addSecondCategory',
+      type:'post',
+      data:$('#form').serialize(),
+      success:function(info){
+        // console.log(info);
+        if(info.success){
+          $('#secondModal').modal('hide');
+          currentPage=1;
+          render();
+          $('#form').data('bootstrapValidator').resetForm(true);
+          //因为一级下拉菜单和图片不是表单元素，所以重置不了需要手动进行设置
+          $('#droptext').text('请输入一级分类');
+          $('#imgBox img').attr('src','images/none.png');
+        }
+      }
+    })
+  })
 
 
 
